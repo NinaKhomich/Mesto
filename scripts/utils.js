027350cb0a27e566a32418c2526ffe1db8ba309d
NinaKhomich <kh22nina@gmail.com>
2023-04-09 18:55:04 +0300

    Updates

--- scripts/utils.js
+++
@@ -1,6 +0,0 @@
-const cards = document.querySelector('.cards');
-const popupPhotoView = document.querySelector('.popup_type_photo');
-const popupPhoto = popupPhotoView.querySelector('.popup__photo');
-const popupPhotoTitle = popupPhotoView.querySelector('.popup__img-title');
-
-export { cards, popupPhotoView, popupPhotoTitle, popupPhoto };
